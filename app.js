6d8d7bdafbccffb9afeced84e976f249f5fe9f74
jsangalang93 <jsangalang93@gmail.com>
2024-04-10 16:15:28 -0400

    commit through console log button message

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 // V buttonElement is your element on JS query slecting bigButton class in css
 
-const buttonElement = document.querySelector("bigButton");
-    console.dir(buttonElement);
+const buttonElement = document.getElementById("bigButton");
+const winCookieElement = 'You win a cookie!'
+    
 
+buttonElement.addE
+
+buttonElement.addEventListener('click', () => {
+    winCookieElement.textContent = buttonElement
+    console.log('you win a cookie! Congratulations!');
+}) 

95f9b741c81063bbbb0d9c0b5bc1daef55c02f5d
jsangalang93 <jsangalang93@gmail.com>
2024-04-11 10:57:41 -0400

    commit through working cookie counter

--- app.js
+++ app.js
@@ -4,35 +4,24 @@ const buttonElement = document.getElementById("bigButton");
 const winCookieElement = 'You win a cookie!'
 const cookieDisplayElement = document.getElementById("cookieDisplay")
 const counterDisplayElement = document.querySelector(".counter")
-const cookieUpdateElement = `you currently have ${'counterElement'} cookies.`
+
 // const counterElementOutput = cookieCounterElement
-
-
 let counterElement = 0;
 
 
 //output, you win a cookie 
 buttonElement.addEventListener('click', () => {
+    // let counterElement = 0;
     cookieDisplayElement.textContent = winCookieElement;
-        // const message = document.createElement("h1");
-        // message.textContent = "you win a cookie!"
-    //^ console.log('you win a cookie! Congratulations!');
+    //^ console.log('you win a cookie! Congratulations!')
+
     //v cookie counter
-    counterElement = counterElement + 1;
-        console.log(counterElement);
-    // counterDisplayElement.textContent = `you currently have ${'counterElement'} cookies.`;
-        console.dir(counterDisplayElement);
-    counterDisplayElement.textContent = cookieUpdateElement;
-     cookieDisplayElement.appendChild(message);
-        console.log('does this work');
+        counterElement = counterElement + 1
+            console.log(counterElement);
     
-
-    // counterElementOutput.textContent = CounterElement;
-    // counterElementOutput.textContent = `you currently have ${'cooCounterElement'} cookies.`;
-    // cookieCounterElement.textContent = CounterElementOutput;
-    // cooCounterElement.textContent = `you currently have ${'CounterElement'} cookies.`;
+    const cookieUpdateElement = `you currently have ${counterElement} cookies.`
+    counterDisplayElement.textContent = cookieUpdateElement;
 }) 
-
 
 
 

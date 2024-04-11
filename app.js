03a6befc8131d0fce952c048759fae9846601023
jsangalang93 <jsangalang93@gmail.com>
2024-04-11 09:51:57 -0400

    cookie counter debug wip

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 // V buttonElement is your element on JS query slecting bigButton class in css
 
 const buttonElement = document.getElementById("bigButton");
-const winCookieElement = 'HA-CHA! You win a cookie!'
+const winCookieElement = 'You win a cookie!'
 const cookieDisplayElement = document.getElementById("cookieDisplay")
-const cooCounterElement = document.getElementById("counter")
+const counterDisplayElement = document.querySelector(".counter")
+const cookieUpdateElement = `you currently have ${'counterElement'} cookies.`
 // const counterElementOutput = cookieCounterElement
 
 
@@ -13,18 +14,26 @@ let counterElement = 0;
 //output, you win a cookie 
 buttonElement.addEventListener('click', () => {
     cookieDisplayElement.textContent = winCookieElement;
+        // const message = document.createElement("h1");
+        // message.textContent = "you win a cookie!"
     //^ console.log('you win a cookie! Congratulations!');
     //v cookie counter
     counterElement = counterElement + 1;
-    console.log(counterElement);
-    // counterElementOutput.textContent = cookieCounterElement;
+        console.log(counterElement);
+    // counterDisplayElement.textContent = `you currently have ${'counterElement'} cookies.`;
+        console.dir(counterDisplayElement);
+    counterDisplayElement.textContent = cookieUpdateElement;
+     cookieDisplayElement.appendChild(message);
+        console.log('does this work');
+    
+
+    // counterElementOutput.textContent = CounterElement;
     // counterElementOutput.textContent = `you currently have ${'cooCounterElement'} cookies.`;
     // cookieCounterElement.textContent = CounterElementOutput;
     // cooCounterElement.textContent = `you currently have ${'CounterElement'} cookies.`;
 }) 
 
 
-cooCounterElement.textContent = `you currently have ${'CounterElement'} cookies.`;
 
 
 

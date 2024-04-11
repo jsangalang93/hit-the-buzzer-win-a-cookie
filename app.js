77f829f9c0d4df3ecb683579863c7ee3744caf69
jsangalang93 <jsangalang93@gmail.com>
2024-04-10 20:59:39 -0400

    cookie counter through console log counter still no print

--- app.js
+++ app.js
@@ -3,13 +3,28 @@
 const buttonElement = document.getElementById("bigButton");
 const winCookieElement = 'HA-CHA! You win a cookie!'
 const cookieDisplayElement = document.getElementById("cookieDisplay")
+const cooCounterElement = document.getElementById("counter")
+// const counterElementOutput = cookieCounterElement
 
 
+let counterElement = 0;
+
+
+//output, you win a cookie 
 buttonElement.addEventListener('click', () => {
-    cookieDisplayElement.textContent = winCookieElement
-    console.log('you win a cookie! Congratulations!');
+    cookieDisplayElement.textContent = winCookieElement;
+    //^ console.log('you win a cookie! Congratulations!');
+    //v cookie counter
+    counterElement = counterElement + 1;
+    console.log(counterElement);
+    // counterElementOutput.textContent = cookieCounterElement;
+    // counterElementOutput.textContent = `you currently have ${'cooCounterElement'} cookies.`;
+    // cookieCounterElement.textContent = CounterElementOutput;
+    // cooCounterElement.textContent = `you currently have ${'CounterElement'} cookies.`;
 }) 
 
+
+cooCounterElement.textContent = `you currently have ${'CounterElement'} cookies.`;
 
 
 

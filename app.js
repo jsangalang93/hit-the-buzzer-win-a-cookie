a5466789da4e7a18de7da530e6ae2382bcb7fa37
jsangalang93 <jsangalang93@gmail.com>
2024-04-12 12:35:34 -0400

    cookie choices wip

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const counterDisplayElement = document.querySelector(".counter")
 const backgroundElement = document.querySelector(".background")
 const CookiePop = document.querySelector(".cookieGrid")
 const eatElement = document.getElementById("eatCookie")
+//COOKIE CHOICES PLAYAROUND
+const cookieChoiceElement = document.querySelector(".cookieChoice")
 // OUTPUT CONSTANTS
 
 
@@ -46,12 +48,24 @@ buttonElement.addEventListener('click', () => {
     counterElement = counterElement + 1
 
     const cookieUpdateElement = `you currently have ${counterElement} cookies.`
-    // counterDisplayElement.textContent = cookieUpdateElement;
+    counterDisplayElement.textContent = cookieUpdateElement;
         if(counterElement < 10 || counterElement === 10){
-            cookieDisplayElement.textContent = `Ha-Cha! You win a cookie!`
-            counterDisplayElement.textContent = cookieUpdateElement;
-            ding.volume = 1
-            ding.play()
+            ding.volume = 1;
+            ding.play();
+            cookieDisplayElement.textContent = `Ha-Cha! You win a cookie! What kind of cookie would you like?`
+            // // appendchild three buttons, sugar, chocolate, bday
+            //     if(cookieChoiceElement === "Sugar Cookie") {
+            //         console.log(choclate chip)
+
+            //     } else if(cookieChoiceElement === "Chocolate Chip") {
+            //         console.log("chocolate chip works")
+
+            //     } else {
+            //         console.log("birthday cookie works");
+            //     }
+
+            // counterDisplayElement.textContent = cookieUpdateElement;
+            
         }else{
             buttonElement.removeEventListener('click', buttonElement)
             diabetesElement.textContent = 'You seem to have a lot on your plate! Eat one before you test your luck again!'
@@ -78,5 +92,6 @@ eatElement.addEventListener('click', () => {
 
 // cookie counter
 
+// if()
 
 

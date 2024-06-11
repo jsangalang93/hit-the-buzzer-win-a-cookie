4905da27136349601ea7427c7daa5b2d1b20006a
jsangalang93 <jsangalang93@gmail.com>
2024-06-11 11:20:39 -0400

    working on translation, want to make sure original file is functional

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
 // V buttonElement is your element on JS query slecting bigButton class in css
 
-const buttonElement = document.getElementById("bigButton")
-const cookieDisplayElement = document.getElementById("cookieDisplay")
-const counterDisplayElement = document.querySelector(".counter")
-const backgroundElement = document.querySelector(".background")
-const CookiePop = document.querySelector(".cookieGrid")
-const eatElement = document.getElementById("eatCookie")
-const mainGridElement = document.querySelector(".mainGrid")
-const startMusicElement = document.querySelector(".play")
-const pauseMusicElement = document.querySelector(".pause")
+// const buttonElement = document.getElementById("bigButton")
+// const cookieDisplayElement = document.getElementById("cookieDisplay")
+// const counterDisplayElement = document.querySelector(".counter")
+// const backgroundElement = document.querySelector(".background")
+// const CookiePop = document.querySelector(".cookieGrid")
+// const eatElement = document.getElementById("eatCookie")
+// const mainGridElement = document.querySelector(".mainGrid")
+// const startMusicElement = document.querySelector(".play")
+// const pauseMusicElement = document.querySelector(".pause")
+// // what kind of cookie do you want
+// const whatKindElement = document.querySelector(".whatKind")
+
+// // CONST TO DISPLAY TOO MANY COOKIES PROMPT
+// const diabetesElement = document.getElementById("tooMany")
+
+const buttonElement = $("bigButton")
+const cookieDisplayElement = $("cookieDisplay")
+const counterDisplayElement = $(".counter")
+const backgroundElement = $(".background")
+const CookiePop = $(".cookieGrid")
+const eatElement = $("eatCookie")
+const mainGridElement = $(".mainGrid")
+const startMusicElement = $(".play")
+const pauseMusicElement = $(".pause")
 // what kind of cookie do you want
-const whatKindElement = document.querySelector(".whatKind")
+const whatKindElement = $(".whatKind")
 
 // CONST TO DISPLAY TOO MANY COOKIES PROMPT
-const diabetesElement = document.getElementById("tooMany")
+const diabetesElement = $("tooMany")
 
 //AUDIO CONSTANTS
 const ding = new Audio("sounds/bell.mp3")
@@ -33,18 +48,28 @@ const backgroundMusic = new Audio("sounds/background music.mp3")
 
 
 //PLATE PHOTO 
-const image = document.createElement("img");
-image.src = "images/elegant-plate-p-4000.png";
-document.querySelector(".cookiePlate").appendChild(image)
-image.classList.add("plateResize")
+$('<img>', {
+    src: 'images/elegant-plate-p-4000.png',
+    class: 'plateResize'
+
+}).appendTo('.cookiePlate')
+// const image = document.createElement("img");
+// image.src = "images/elegant-plate-p-4000.png";
+// document.querySelector(".cookiePlate").appendChild(image)
+// image.classList.add("plateResize")
 
 
 
 
 //BACKGROUND IMAGE
-const backgroundImage = document.createElement("img");
-backgroundImage.src = "images/background.jpg"
-document.querySelector(".mainGrid").appendChild(backgroundImage)
+$('<img>', {
+    src: 'images/background.jpg',
+    class: 'background'
+
+}).appendTo('.mainGrid')
+// const backgroundImage = document.createElement("img");
+// backgroundImage.src = "images/background.jpg"
+// document.querySelector(".mainGrid").appendChild(backgroundImage)
 
 //adding class to background element, styling
 backgroundImage.classList.add('background')
@@ -63,30 +88,31 @@ const functionButton = () => {
 
 
 // EAT A COOKIE BUTTON
-eatElement.addEventListener('click', () => {
+$('#eatElement').on('click', function() {
     
     if (counterElement <= 10 && counterElement >= 1){
         counterElement = counterElement - 1
-        const cookieUpdateElement = `you currently have ${counterElement} cookies.`
-    cookieDisplayElement.textContent = `mmmmm Hope you enjoyed!`
-            counterDisplayElement.textContent = cookieUpdateElement;
+        const cookieUpdateElement = `you currently have ${counterElement} cookies.`;
+        $('cookieDisplayElement').text(`mmmmm Hope you enjoyed!`)
+        $('counterDisplayElement').text(cookieUpdateElement);
 
-            const appendCookiesElement = document.querySelector(".appendCookies")
-            appendCookiesElement.removeChild(appendCookiesElement.firstElementChild);
-            diabetesElement.textContent = '';
-            munch.volume = 1
-            munch.play()
+        const appendCookiesElement = $(".appendCookies");
+        appendCookiesElement.children().first().remove();
+        $('diabetesElement').text('');
+        munch.volume = 1
+        munch.play()
     }
     // console.log("this is the cookie button", counterElement);
 });
 
 //MAIN EVENT
-buttonElement.addEventListener('click', () => {
+// buttonElement.addEventListener('click', () => {
+$('#buttonElement').on('click', function() {
     
     // counterDisplayElement.textContent = cookieUpdateElement;
         if(counterElement < 9){
 
-            const buttonElement = document.getElementById("bigButton")
+            const buttonElement = $("bigButton")
                 counterElement = counterElement + 1
             const cookieUpdateElement = `you currently have ${counterElement} cookies.`
             // buttonElement.remove();
@@ -94,7 +120,7 @@ buttonElement.addEventListener('click', () => {
                 // console.log("this is the counter", counterElement)
             cookieDisplayElement.textContent = `Ha-Cha! You win a cookie! What kind of cookie would you like?`
                 // button disappear
-            document.getElementById('bigButton').style.display= 'none';
+            $('bigButton').style.display= 'none';
                 // audio play 
                 ding.volume = 1
                 ding.play()
